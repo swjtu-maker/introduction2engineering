8f2d5689910d26a3b0e1d7717f75e7102c0afa33
bloomlj <kollocc@live.com>
2020-09-19 23:20:57 +0800

    实测，可用

---
+++ LoRa_Gateway_MQTT/LoRa_Gateway_MQTT.ino
@@ -0,0 +1,242 @@
+/*
+  LoRaNow Simple Gateway with ESP32 setPins
+
+  This code creates a webServer show the Message and use mqtt publish the data.
+  
+  created 27 04 2019
+  by Luiz H. Cassettari
+  modified 2020.09.19
+  by bloomlj 
+*/
+
+#include <LoRaNow.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <StreamString.h>
+#include <PubSubClient.h>
+
+//vspi
+//#define SCK 18
+//#define MISO 19
+//#define MOSI 23
+//#define SS 5
+
+//hspi
+#define SCK 14
+#define MISO 12
+#define MOSI 13
+#define SS 15
+
+#define DIO0 4
+
+const char *ssid = "TY666";
+const char *password = "fishfish";
+const char* mqtt_server = "broker.hivemq.com";
+
+WebServer server(80);
+WiFiClient espClient;
+PubSubClient mqttclient(espClient);
+
+const char *script = "<script>function loop() {var resp = GET_NOW('loranow'); var area = document.getElementById('area').value; document.getElementById('area').value = area + resp; setTimeout('loop()', 1000);} function GET_NOW(get) { var xmlhttp; if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest(); else xmlhttp = new ActiveXObject('Microsoft.XMLHTTP'); xmlhttp.open('GET', get, false); xmlhttp.send(); return xmlhttp.responseText; }</script>";
+
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE  (50)
+char msg[MSG_BUFFER_SIZE];
+
+int value = 0;
+
+void handleRoot()
+{
+  String str = "";
+  str += "<html>";
+  str += "<head>";
+  str += "<title>ESP32 - LoRaNow</title>";
+  str += "<meta name='viewport' content='width=device-width, initial-scale=1'>";
+  str += script;
+  str += "</head>";
+  str += "<body onload='loop()'>";
+  str += "<center>";
+  str += "<textarea id='area' style='width:800px; height:400px;'></textarea>";
+  str += "</center>";
+  str += "</body>";
+  str += "</html>";
+  server.send(200, "text/html", str);
+}
+
+static StreamString string;
+
+void handleLoRaNow()
+{
+  server.send(200, "text/plain", string);
+  while (string.available()) // clear
+  {
+    string.read();
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    //digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else {
+    //digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void setup(void)
+{
+
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+  if (ssid != "")
+    WiFi.begin(ssid, password);
+  WiFi.begin();
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handleRoot);
+  server.on("/loranow", handleLoRaNow);
+  server.begin();
+  Serial.println("HTTP server started");
+
+  LoRaNow.setFrequencyCN(); // Select the frequency 486.5 MHz - Used in China
+  // LoRaNow.setFrequencyEU(); // Select the frequency 868.3 MHz - Used in Europe
+  // LoRaNow.setFrequencyUS(); // Select the frequency 904.1 MHz - Used in USA, Canada and South America
+  // LoRaNow.setFrequencyAU(); // Select the frequency 917.0 MHz - Used in Australia, Brazil and Chile
+
+  // LoRaNow.setFrequency(frequency);
+  // LoRaNow.setSpreadingFactor(sf);
+  // LoRaNow.setPins(ss, dio0);
+
+  LoRaNow.setPinsSPI(SCK, MISO, MOSI, SS, DIO0); // Only works with ESP32
+
+  if (!LoRaNow.begin())
+  {
+    Serial.println("LoRa init failed. Check your connections.");
+    while (true)
+      ;
+  }
+
+  LoRaNow.onMessage(onMessage);
+  LoRaNow.gateway();
+  //mqtt
+  mqttclient.setServer(mqtt_server, 1883);
+  mqttclient.setCallback(callback);
+  
+}
+
+void loop(void)
+{
+  LoRaNow.loop();
+  server.handleClient();
+  mqttloop();
+}
+
+void onMessage(uint8_t *buffer, size_t size)
+{
+  unsigned long id = LoRaNow.id();
+  byte count = LoRaNow.count();
+
+  Serial.print("Node Id: ");
+  Serial.println(id, HEX);
+  Serial.print("Count: ");
+  Serial.println(count);
+  Serial.print("Message: ");
+  Serial.write(buffer, size);
+  Serial.println();
+  Serial.println();
+
+   snprintf (msg, MSG_BUFFER_SIZE, "lora_MSG_COUNT #%ld", count);
+   Serial.print("Publish message: ");
+   Serial.println(msg);
+   mqttclient.publish("C2TLOutTopic", msg);
+      
+  if (string.available() > 512)
+  {
+    while (string.available())
+    {
+      string.read();
+    }
+  }
+
+  string.print("Node Id: ");
+  string.println(id, HEX);
+  string.print("Count: ");
+  string.println(count);
+  string.print("Message: ");
+  string.write(buffer, size);
+  string.println();
+  string.println();
+
+  // Send data to the node
+  LoRaNow.clear();
+  LoRaNow.print("LoRaNow Gateway Message ");
+  LoRaNow.print(millis());
+  LoRaNow.send();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!mqttclient.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random mqttclient ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (mqttclient.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      mqttclient.publish("C2TLOutTopic", "hello world");
+      // ... and resubscribe
+      mqttclient.subscribe("C2TLInTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqttclient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void mqttloop() {
+
+  if (!mqttclient.connected()) {
+    reconnect();
+  }
+  mqttclient.loop();
+
+//  unsigned long now = millis();
+//  if (now - lastMsg > 2000) {
+//    lastMsg = now;
+//    ++value;
+//    snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+//    Serial.print("Publish message: ");
+//    Serial.println(msg);
+//    mqttclient.publish("C2TLOutTopic", msg);
+//  }
+}
